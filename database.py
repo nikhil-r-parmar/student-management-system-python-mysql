3e179c3a54699ea80f88e9f794e10dc922d64c6e
Nikhil R. Parmar <nikhilparmarofc@gmail.com>
2026-07-06 16:50:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import mysql.connector
 connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="12345",
+    password="your_database_password",
     database="school_db"
 )
 
